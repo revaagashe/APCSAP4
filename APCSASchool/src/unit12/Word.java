a8cc657ef29bf9eacf2cac6b736723abdcb4281f
agasher7589 <agasher7589@CA-SU-F106-35.sduhsd.lan>
2018-03-15 13:43:08 -0700

    aasdf

---
+++ APCSASchool/src/unit12/Word.java
@@ -0,0 +1,28 @@
+package unit12;
+
+import static java.lang.System.*;
+
+public class Word implements Comparable<Word>
+{
+	private String word;
+
+	public Word( String s )
+	{
+		word = s;
+
+	}
+
+	public int compareTo( Word rhs )
+	{
+		
+		
+		
+		
+		return 0;
+	}
+
+	public String toString()
+	{
+		return "";
+	}
+}
